b39ba51e716c134ccebacf3e0fb10ec0b6ca5db2
Zerapium <mayurhiwale2451982@gmail.com>
2024-02-02 23:16:12 +0530

    temporary hide editor on mobile view

--- frontend/src/components/edit-assignment.js
+++ frontend/src/components/edit-assignment.js
@@ -16,8 +16,77 @@ renderer.code = function (code, language) {
 marked.setOptions({ renderer });
 
 function App() {
-  const [text, setText] = useState("");
-  const [markdown, setMarkdown] = useState("");
+  let mkdw = `
+
+  <b> <i> This feature is not completely done implementing yet, so here is a sample preview </i> </b>
+
+  # BIAS
+  
+  BIAS is a Bot for Instant Assignment Solving.
+  
+  
+  Link : http://65.0.14.141/
+  
+  BIAS is a webapp that provides an automation tool to generate answers to your assignments, host and organise your assignments, experiments and other important study material.
+  
+  It uses multiple AIs to generate textual answers for the given questions and gives you a well structured downloadable PDF in few seconds.
+  
+  
+  
+  ## Features
+  
+  - Easy-to-use
+  - Well organised study material
+  - Generate accurate assignment answers
+  - View Submission Dates
+  - Variety of assignments available
+  - Download Notes/Assignments or view it on webapp itself
+  - Working on more.
+  
+  
+  ## FAQ
+  
+  #### 1) Is this an official college-approved webapp?
+  
+  No, this is a personal experimental project and is not approved/supported by any kind of institue authorities.
+  
+  #### 2) Will i get full marks if i write answers from the generated PDF?
+  
+  No, we do NOT guarantee you full marks. No teacher gives out full marks easily even if you write from the textbook itself.
+  
+  #### 3) Does this AI generate diagrams/images in the PDF?
+  
+  As of now, our webapp does NOT include any kind of graphical content (including diagrams and images) in the generated PDF.
+  
+  #### 4) What are Phoenix's thoughts about this?
+  
+  Well, does it matter? As we all know Phoenix's E ability (fireball) is to heal himself while damaging others around. In a similar way, for his own 'academic' satisfaction, he makes us suffer... JUST KIDDING! HAH!
+  
+  
+  
+  ## Documentation
+  
+  [API Reference](https://github.com/ISenseAura/Assignment-Buddy-API)
+  
+  
+
+  ## Credits
+  
+  ### Owners
+  
+  - [Mayur Hiwale](https://www.github.com/isenseaura) - Lead Developer | Backend Development
+  - [Om Patil](https://www.github.com/potato-lulw) - UI/UX Design | Frontend Development
+  
+  ###  Contributors
+  - [Shashank Bhave](https://github.com/CAPTAINxNEMO) - Frontend Development | [Feature Implementation](https://github.com/potato-lulw/assignment-buddy/issues/6)
+  
+  - [Himanshu Ranjan](https://www.github.com/RanjanH) - Bug Report
+  `
+
+// mkdw is just sample mkdw, please change it with the content received from the server in future
+
+  const [text, setText] = useState(mkdw);
+  const [markdown, setMarkdown] = useState(marked(mkdw));
 
   const handleChange = (event) => {
     setText(event.target.value);
@@ -43,6 +112,7 @@ function App() {
   //     console.error(err);
   //   }
   // }
+
 
   return (
     <div className="edit-assignment-container">
